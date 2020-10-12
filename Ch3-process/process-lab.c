ea87f26c1705f8196f684b3026ac325fc1f75e02
kuma0416 <a24350845@gmail.com>
2020-10-13 00:48:39 +0800

    Update ver 2

--- Ch3-process/process-lab.c
+++ Ch3-process/process-lab.c
@@ -7,35 +7,31 @@
 int main(void){
     char *args[MAX_LINE/2+1];
     int should_run = 1;
-    char string[80], string_buf[10][80];
+    char string[80];
     while(should_run){
         printf("osh>");
         fflush(stdout);
 
         int length, argsSE = 0;
+        char string_buf[10][80];
         fgets(string, MAX_LINE, stdin);
         length = strlen(string);
-        while(scanf("%s", &string)!= EOF){
-            printf("%s", string);
-        }
-        //printf("length: %d, string: %s\n", length, string);
-        //args[0] = string; 
-        //printf("args test: %s", args[0]);
-        /*for(int i=0; i< length; ++i){
-            printf("before: %c\n", string[i]);
-            if(string[i]==' ' || string[i] == '\0'){
+        char *start = string;
+        char *space;
+        printf("%s", string);
+        while(start!=NULL){
+            space = strchr(start, ' ');
+            if(space == NULL){
+                strcpy(string_buf[argsSE], start);
                 ++argsSE;
+                start = NULL;
             } else {
-                strcat( string_buf[argsSE], &string[i]);
+                strncpy(string_buf[argsSE], start, space - start);
+                string_buf[argsSE][space - start] = '\0';
+                ++argsSE;
+                start = space + 1;
             }
-            printf("after: string_buf->%s, argsSE: %d\n", string_buf[argsSE], argsSE);
-        }*/
-        /*for(int i=0; i<2; ++i){
-            printf("string buf: %s\n", &string_buf[i]);
-        }*/
-        /*char a[10] = "a";
-        strcat(string, a);
-        printf("args test: %s, string: %s", args[0], string);*/
+        }
     }
     return 0;
 }
